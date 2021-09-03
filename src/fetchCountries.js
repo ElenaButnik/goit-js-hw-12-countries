ee639dde4622aa1b367a9607e5e0ebc7cfe0010d
ElenaButnik <butnik.elena@gmail.com>
2021-09-03 23:27:57 +0300

    оформлен выбор стран

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,14 +3,41 @@ import countryCard from './templates/country-card.hbs';
 
 
 const refs = {
-    countryContainer: document.querySelector('.js-input-form'),
+    countryContainer: document.querySelector('.js-card-container'),
+    searchForm: document.querySelector('.js-input-form'),
+};
+
+refs.searchForm.addEventListener('input', onSearchInput);
+//let SearchQuery = " ";
+
+
+function onSearchInput (e) {
+    //e.preventDefault();
+
+    //const form = e.currentTarget;
+   // console.log(form.elements);
+  // let SearchQuery = form.elements.query.value;
+  // const input = e.currentTarget;
+ 
+ let  SearchQuery = refs.searchForm.value;
+
+fetchCountries(SearchQuery)
+.then(renderCountries)
+.catch(err => console.log(err));
 }
 
-fetch('https://restcountries.eu/rest/v2/alpha/col')
-.then(response => console.log(response.json()))
-.then(country => {
+function fetchCountries(name) {
+ return  fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+
+.then(response => {
+    return response.json();
+} 
+);
+}
+
+function renderCountries(country) {
     const markup = countryCard(country);
-    console.log(markup);
     refs.countryContainer.innerHTML = markup;
-})
-.catch(err => console.log(err))
+}
+
+
